d11a33948a15efad646f0986ff0cf070987bd3a3
Valentin Massonniere <valentin78.massonniere@gmail.com>
2024-04-28 01:26:10 +0200

    [Installation on a Virtual Machine] update readme and create a deploy.sh for do a cron task on a linux vn

--- linux/deploy.sh
+++ linux/deploy.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 
-# Install Docker
-sudo apt-get update
-sudo apt-get install -y docker.io
+# Check if Docker is already installed
+if ! command -v docker &> /dev/null; then
+    # Install Docker
+    sudo apt-get update
+    sudo apt-get install -y docker.io
+    echo "Docker installed successfully."
+else
+    echo "Docker is already installed."
+fi
 
-# Install Docker Compose
-sudo apt-get install -y docker-compose
+# Check if Docker Compose is already installed
+if ! command -v docker-compose &> /dev/null; then
+    # Install Docker Compose
+    sudo apt-get update
+    sudo apt-get install -y docker-compose
+    echo "Docker Compose installed successfully."
+else
+    echo "Docker Compose is already installed."
+fi
 
 # Check if the folder exists
 if [ -d "TheVal-Discord-Bot" ]; then
-    echo "Le dossier existe déjà. Arrêt et suppression des conteneurs Docker existants..."
+    echo "TheVal-Discord-Bot repository already exists. Stopping and removing existing Docker containers..."
     # Stop and remove existing Docker containers
     docker-compose -f "TheVal-Discord-Bot/docker-compose.yml" down
 else
     # Clone the repository if it doesn't exist
     git clone https://github.com/TheValll/TheVal-Discord-Bot
-    cd TheVal-Discord-Bot
+    cd TheVal-Discord-Bot || exit
+    echo "TheVal-Discord-Bot repository cloned successfully."
 fi
 
 # Build the Docker image
@@ -23,3 +37,5 @@ docker build --pull --rm -f "dockerfile" -t bot:latest "."
 
 # Start the Docker containers
 docker-compose -f "docker-compose.yml" up -d --build
+
+echo "TheVal Bot installation completed successfully."
